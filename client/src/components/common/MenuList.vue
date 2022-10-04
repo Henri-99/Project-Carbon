85dc33cd6fd2d417254001679169c15e85bce68e
digable-dissenter <this_is_skimps@hotmail.co.uk>
2022-10-04 20:44:43 +0200

    Amina's changes

---
+++ client/src/components/common/MenuList.vue
@@ -0,0 +1,53 @@
+<template>
+    <ul class="menu-list ms-lg-auto">
+          <li class="menu-item has-sub">
+              <a href="#" class="menu-link menu-toggle">{{ SectionData.headerData.menuList.title }}</a>
+              <div class="menu-sub">
+                 <ul class="menu-list">
+                      <li class="menu-item" v-for="nav in SectionData.headerData.menuList.navList" :key="nav.id"><router-link :to="nav.path" class="menu-link">{{ nav.title }} <span :class="nav.badgeClass" v-if="nav.badge">{{ nav.badge }}</span></router-link></li>
+                 </ul>
+              </div>
+          </li>
+          <li class="menu-item has-sub">
+              <a href="#" class="menu-link menu-toggle">{{ SectionData.headerData.menuList2.title }}</a>
+              <div class="menu-sub">
+                 <ul class="menu-list">
+                      <li class="menu-item" v-for="nav in SectionData.headerData.menuList2.navList" :key="nav.id"><router-link :to="nav.path" class="menu-link">{{ nav.title }} <span :class="nav.badgeClass" v-if="nav.badge">{{ nav.badge }}</span></router-link></li>
+                 </ul>
+              </div>
+          </li>
+          <li class="menu-item has-sub">
+              <a href="#" class="menu-link menu-toggle">{{ SectionData.headerData.menuList3.title }}</a>
+              <div class="menu-sub">
+                 <ul class="menu-list">
+                      <li class="menu-item" v-for="nav in SectionData.headerData.menuList3.navList" :key="nav.id"><router-link :to="nav.path" class="menu-link">{{ nav.title }}</router-link></li>
+                 </ul>
+              </div>
+          </li>
+          <li class="menu-item has-sub">
+              <a href="#" class="menu-link menu-toggle">{{ SectionData.headerData.menuList4.title }}</a>
+              <div class="menu-sub ">
+                
+                    <ul class="menu-list">
+                        <li class="menu-item" v-for="nav in SectionData.headerData.menuList4.navList" :key="nav.id"><router-link :to="nav.path" class="menu-link">{{ nav.title }} <span :class="nav.badgeClass" v-if="nav.badge">{{ nav.badge }}</span></router-link></li>
+                    </ul>
+                   
+                </div>
+             
+          </li>
+     </ul>
+</template>
+
+<script>
+// Import component data. You can change the data in the store to reflect in all component
+import SectionData from '@/store/store.js'
+
+export default {
+  name: 'MenuList',
+  data () {
+    return {
+      SectionData
+    }
+  }
+}
+</script>
---
+++ client/src/pages/ItemDetails.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="page-wrap">
+    <!-- header  -->
+    <header class="header-section has-header-main">
+        <!-- Header main -->
+        <HeaderMain></HeaderMain>
+    </header>
+    <!-- item detail section -->
+    <ItemDetailSection></ItemDetailSection>
+   
+    <!-- Footer  -->
+    <Footer classname="bg-dark on-dark"></Footer>
+</div><!-- end page-wrap -->
+</template>
+
+<script>
+export default {
+  name: 'ItemDetails',
+  
+}
+</script>
